2750e3a008d5907689f97c548b52837f1fa2b6f5
acgotaku <acgotaku311@gmail.com>
2024-11-23 23:27:31 +0900

    feat: init project

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { StoreProvider, store } from '@/stores';
+import '@/styles/index.css';
+import App from '@/App';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <StoreProvider value={store}>
+      <App />
+    </StoreProvider>
+  </React.StrictMode>
+);
